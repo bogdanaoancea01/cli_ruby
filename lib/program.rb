b86ca15628c562c17ee42853b75c46fedbd9ebc6
bogdanaoancea <bogdanaoancea01@gmail.com>
2026-06-23 11:48:24 +0300

    Added command factory for managing cli commands

--- lib/program.rb
+++ lib/program.rb
@@ -1,25 +1,24 @@
 require './lib/program_result'
 require './lib/gem_info'
 require './lib/api_client'
+require './lib/commands/command_factory'
+require './lib/commands/command'
 
 class Program
     def execute(args)
 
-        command = args[0]
-        gem_name = args[1]
+        if args.empty?
+            return ProgramResult.new(1, 'No command given')
+        end
+
+        command_name = args[0]
+
+        command = CommandFactory.find(command_name)
 
         if command.nil?
-            ProgramResult.new(1, "No command given")
-        elsif !command.eql?("show")
-            ProgramResult.new(2, "Command unknown")
-            
-        elsif gem_name.nil?
-            ProgramResult.new(3, "No gem name given")
-        else
-            gem_json = APIClient.show(gem_name)
-            gem_object = GemInfo.new(gem_json['name'], gem_json['info'])
-
-            ProgramResult.new(0, "Gem name: #{gem_object.name} Gem info: #{gem_object.info}")
+            return ProgramResult.new(2, 'Command unknown')
         end
+
+        command.execute(args[1])
     end
 end
