13a287144212cd243b00cf4563738bfd6c6177e0
bogdanaoancea <bogdanaoancea01@gmail.com>
2026-06-26 10:03:22 +0300

    Fixes after code review

--- spec/lib/show_command_spec.rb
+++ spec/lib/show_command_spec.rb
@@ -10,59 +10,10 @@ RSpec.describe ShowCommand do
       "Gem name: rails\nGem info: Ruby on Rails is a full-stack web framework optimized for programmer happiness and sustainable productivity. It encourages beautiful code by favoring convention over configuration."
     end
     let(:gem_json) do
-      '{
-            "name": "rails",
-            "downloads": 756415561,
-            "version": "8.1.3",
-            "version_created_at": "2026-03-24T20:27:42.098Z",
-            "version_downloads": 7351214,
-            "platform": "ruby",
-            "authors": "David Heinemeier Hansson",
-            "info": "Ruby on Rails is a full-stack web framework optimized for programmer happiness and sustainable productivity. It encourages beautiful code by favoring convention over configuration.",
-            "licenses": [
-                "MIT"
-            ],
-            "metadata": {
-                "changelog_uri": "https://github.com/rails/rails/releases/tag/v8.1.3",
-                "bug_tracker_uri": "https://github.com/rails/rails/issues",
-                "source_code_uri": "https://github.com/rails/rails/tree/v8.1.3",
-                "mailing_list_uri": "https://discuss.rubyonrails.org/c/rubyonrails-talk",
-                "documentation_uri": "https://api.rubyonrails.org/v8.1.3/",
-                "rubygems_mfa_required": "true"
-            },
-            "yanked": false,
-            "sha": "6d017ba5348c98fc909753a8169b21d44de14d2a0b92d140d1a966834c3c9cd3",
-            "spec_sha": "3a316a58ddb9d1ca3ffa17f10af837ba7e0f6e7cc6e413fc4810e89759f149af",
-            "project_uri": "https://rubygems.org/gems/rails",
-            "gem_uri": "https://rubygems.org/gems/rails-8.1.3.gem",
-            "homepage_uri": "https://rubyonrails.org",
-            "wiki_uri": null,
-            "documentation_uri": "https://api.rubyonrails.org/v8.1.3/",
-            "mailing_list_uri": "https://discuss.rubyonrails.org/c/rubyonrails-talk",
-            "source_code_uri": "https://github.com/rails/rails/tree/v8.1.3",
-            "bug_tracker_uri": "https://github.com/rails/rails/issues",
-            "changelog_uri": "https://github.com/rails/rails/releases/tag/v8.1.3",
-            "funding_uri": null,
-            "dependencies": {
-                    "development": [],
-                    "runtime": [
-                    { "name": "actioncable", "requirements": "= 8.1.3" },
-                    { "name": "actionmailbox", "requirements": "= 8.1.3" },
-                    { "name": "actionmailer", "requirements": "= 8.1.3" },
-                    { "name": "actionpack", "requirements": "= 8.1.3" },
-                    { "name": "actiontext", "requirements": "= 8.1.3" },
-                    { "name": "actionview", "requirements": "= 8.1.3" },
-                    { "name": "activejob", "requirements": "= 8.1.3" },
-                    { "name": "activemodel", "requirements": "= 8.1.3" },
-                    { "name": "activerecord", "requirements": "= 8.1.3" },
-                    { "name": "activestorage", "requirements": "= 8.1.3" },
-                    { "name": "activesupport", "requirements": "= 8.1.3" },
-                    { "name": "bundler", "requirements": "\u003E= 1.15.0" },
-                    { "name": "railties", "requirements": "= 8.1.3" }
-                    ]
-            }
-            }'
+      File.read('spec/fixtures/gem_json.json')
     end
+    let(:non_existent_gem) { 'bogdana' }
+    let(:gem) { 'rails' }
 
     it 'returns exit code 3 when no gem name is given' do
       result = command.execute(nil)
@@ -74,19 +25,19 @@ RSpec.describe ShowCommand do
     it 'returns gem not found for a gem that does not exist' do
       allow(client)
         .to receive(:show)
-        .with('bogdana')
+        .with(non_existent_gem)
         .and_return('Gem not found')
 
-      result = command.execute('bogdana')
+      result = command.execute(non_existent_gem)
 
       expect(result.exit_code).to eq(4)
       expect(result.exit_description).to eq('Gem not found')
     end
 
-    it 'returns information for rails' do
-      allow(client).to receive(:show).with('rails').and_return(JSON.parse(gem_json))
+    it 'returns information for rails gem' do
+      allow(client).to receive(:show).with(gem).and_return(JSON.parse(gem_json))
 
-      result = command.execute('rails')
+      result = command.execute(gem)
 
       expect(result.exit_code).to eq(0)
       expect(result.exit_description).to eq(exit_description)
