4587849f375b4b607c2cf438ed99b46c2a273adb
bogdanaoancea <bogdanaoancea01@gmail.com>
2026-06-26 10:03:22 +0300

    Fixes after code review

--- lib/api_client.rb
+++ lib/api_client.rb
@@ -3,17 +3,27 @@
 require 'faraday'
 class APIClient
   BASE_URL = 'https://rubygems.org/api/v1'
+  CONNECTION = Faraday.new do |config|
+    config.response :raise_error
+  end
+
   def self.show(gem_name)
-    response = Faraday.get("#{BASE_URL}/gems/#{gem_name}.json")
-    return 'Gem not found' if response.status == 404
+    response = CONNECTION.get("#{BASE_URL}/gems/#{gem_name}.json")
 
     JSON.parse(response.body)
+  rescue Faraday::ResourceNotFound
+    'Gem not found'
+  rescue Faraday::Error
+    'See status codes for more details'
   end
 
   def self.search(keyword)
-    response = Faraday.get("#{BASE_URL}/search.json?query=#{keyword}")
-    return 'No gems found' if response.status == 404
+    response = CONNECTION.get("#{BASE_URL}/search.json?query=#{keyword}")
 
     JSON.parse(response.body)
+  rescue Faraday::ResourceNotFound
+    'No gems found'
+  rescue Faraday::Error
+    'See status codes for more details'
   end
 end
