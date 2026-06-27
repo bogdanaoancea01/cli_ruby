99f09654bf3d908915dad2b219067c342dcd01de
bogdanaoancea <bogdanaoancea01@gmail.com>
2026-06-27 14:28:45 +0300

    Changed variable name

--- lib/Options/parse_options.rb
+++ lib/Options/parse_options.rb
@@ -4,18 +4,18 @@ require './lib/options/option_info'
 class ParseOptions
     def self.execute(args)
         licence_name = nil
-        is_sort = false
+        sort_by_downloads = false
 
         parser = OptionParser.new
         parser.on('--licence LICENCE_NAME') do |value|
             licence_name = value
         end
         parser.on('--most-downloads-first') do |value|
-            is_sort = true
+            sort_by_downloads = true
         end
 
         parser.parse!(args)
-        OptionInfo.new(licence_name, is_sort)
+        OptionInfo.new(licence_name, sort_by_downloads)
 
     end
 end
